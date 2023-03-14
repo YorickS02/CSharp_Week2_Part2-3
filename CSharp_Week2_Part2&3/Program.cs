3ec2b01fa9b93327917b0b4f44d4c6142d2bda20
YorickSTR <yorick@travpromobile.com>
2023-03-14 23:58:00 +0100

    3-16

---
+++ CSharp_Week2_Part2&3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3-16 ----------------------------------------------------- //
+            List<string> strings = new List<string>();
+
+            strings.Add("First");
+            strings.Add("Second");
+            strings.Add("Third");
+
+          
+            RemoveLast(strings);
+
+            strings.ForEach(Console.WriteLine);
+            
+
+        }
+           // 3-16 ----------------------------------------------------- //
+           public static void RemoveLast(List<string> strings)
+            {
+                if (strings.Count > 0)
+                {
+                    strings.RemoveAt(strings.Count - 1);
+                }
+            }
+
+    }
+}
